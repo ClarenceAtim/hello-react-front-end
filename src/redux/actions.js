00b2df0ae79726efe48d724d6bacedb680055591
Clarence Atim <125105700+ClarenceAtim@users.noreply.github.com>
2023-12-16 21:09:40 +0000

    Remove try/catch

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,12 +2,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const fetchRandomGreeting = createAsyncThunk('greeting/fetchRandomGreeting', async () => {
-  try {
-    const response = await axios.get('http://localhost:3000/random_greeting');
-    return response.data.greeting;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get('http://localhost:3000/random_greeting');
+  return response.data.greeting;
 });
 
 export default fetchRandomGreeting;
